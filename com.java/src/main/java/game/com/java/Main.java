c47a3cf1f9faae031444a712ce06b8fe6d95477b
1599695 <1599695@users.noreply.github.com>
2019-05-25 20:53:35 +0200

    push tests

--- com.java/src/main/java/game/com/java/Main.java
+++ com.java/src/main/java/game/com/java/Main.java
@@ -1,14 +1,15 @@
 package game.com.java;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
-	 public static void main(String[] args) {
-		 LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		 config.title = "Witsiemon";
-		 config.height = 400;
-		 config.width = 600;
-		 config.vSyncEnabled = true;
-		 //new LwjglApplication(new Witsiemon(), config);
-	 }
+ public static void main(String[] args) {
+	 LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+	 config.title = "Witsiemon";
+	 config.height = 400;
+	 config.width = 600;
+	 config.vSyncEnabled = true;
+	 new LwjglApplication(new Witsiemon(), config);
+ }
 }
---
+++ com.java/src/main/java/game/com/java/model/Camera.java
@@ -0,0 +1,19 @@
+package game.com.java.model;
+
+public class Camera {
+	 private float cameraX = 0f;
+	 private float cameraY = 0f;
+	 
+	 public void update(float newCamX, float newCamY) {
+		 this.cameraX = newCamX;
+		 this.cameraY = newCamY;
+	 }
+
+	public float getCameraX() {
+		return cameraX;
+	}
+
+	public float getCameraY() {
+		return cameraY;
+	}
+}
